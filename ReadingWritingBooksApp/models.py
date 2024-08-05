628b73ad9ff6d575e8be5cce3d8e2bac7cbf8156
Bayan Abu Sarari <64931335+Banay97@users.noreply.github.com>
2024-08-05 20:38:04 +0300

    Add files via upload

--- ReadingWritingBooksApp/models.py
+++ ReadingWritingBooksApp/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+import bcrypt
+import re
 
 # Create your models here.
+class UserManager(models.Manager):
+    def user_validator(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = 'First name should be at least 2 characters long'
+
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last name should be at least 2 characters long'
+
+        if len(postData['username']) < 5:
+            errors['username'] = 'Username should be at least 5 characters long'   
+
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not EMAIL_REGEX.match(postData['email']):    # test whether a field matches the pattern            
+            errors['email'] = "Invalid email address!"    
+
+        if len(postData['email']) < 10:
+            errors['email'] = 'Email should be at least 10 characters long'    
+
+        if len(postData['password']) < 8:
+            errors['password'] = 'Password should be at least 8 characters long'
+
+        if postData['password'] != postData['confirm_password']:
+            errors['confirm_password'] = 'Passwords do not match'
+        
+        return errors
+
+class BookManager(models.Manager):
+    def book_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 5:
+            errors['title'] = 'Title should be at least 5 characters long'
+        if len(postData['description']) < 10:
+            errors['description'] = 'Description should be at least 10 characters long'
+        return errors      
+            
+class User(models.Model):
+    ROLE_CHOICES = [
+        ('writer', 'Writer'),
+        ('reader', 'Reader'),
+    ]
+    
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    date_of_birth = models.DateField()
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    bio = models.TextField()
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} - {self.username}'
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    genre =models.CharField(max_length=50)
+    description = models.TextField()
+    author = models.ForeignKey(User, related_name='books', on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = BookManager()
+    
+    def __str__(self):
+        return f'{self.title} - {self.author}'
+    
+        
+

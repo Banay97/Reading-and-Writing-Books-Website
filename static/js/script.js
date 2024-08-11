678f4fa31f17280111c7a8fb438afb13fd428742
Bayan Abu Sarari <64931335+Banay97@users.noreply.github.com>
2024-08-11 09:52:27 +0300

    Add files via upload

--- static/js/script.js
+++ static/js/script.js
@@ -13,3 +13,71 @@ const swiper = new Swiper('.swiper', {
         prevEl: '.swiper-button-prev',
     },
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: 'btn btn-success',
+            cancelButton: 'btn btn-danger'
+        },
+        buttonsStyling: false
+    });
+
+    document.querySelectorAll('button.delete-button').forEach(button => {
+        button.addEventListener('click', function(e) {
+            e.preventDefault();
+            const url = this.getAttribute('data-url');
+            const row = this.closest('tr'); // Find the closest table row
+
+            swalWithBootstrapButtons.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Yes, delete it!',
+                cancelButtonText: 'No, cancel!',
+                reverseButtons: true
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch(url, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value // CSRF token
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                            row.remove(); // Remove the row from the table
+                            swalWithBootstrapButtons.fire({
+                                title: 'Deleted!',
+                                text: 'Your file has been deleted.',
+                                icon: 'success'
+                            });
+                        } else {
+                            swalWithBootstrapButtons.fire({
+                                title: 'Error!',
+                                text: 'Something went wrong.',
+                                icon: 'error'
+                            });
+                        }
+                    })
+                    .catch(error => {
+                        swalWithBootstrapButtons.fire({
+                            title: 'Error!',
+                            text: 'Something went wrong.',
+                            icon: 'error'
+                        });
+                    });
+                } else if (result.dismiss === Swal.DismissReason.cancel) {
+                    swalWithBootstrapButtons.fire({
+                        title: 'Cancelled',
+                        text: 'Your imaginary file is safe :)',
+                        icon: 'error'
+                    });
+                }
+            });
+        });
+    });
+});
